aec24c6f9acf3a36d65eb848acc57ee5665f1395
Matt Wisniewski <hello@mattw.life>
2023-07-12 01:00:54 -0400

    test stubs

---
+++ tests/test_add.py
@@ -0,0 +1,73 @@
+from isyndicate import add_image, add_image_random, add_image_seq, TagSettings
+
+
+def test_add_url_success_minimal():
+    pass
+
+
+def test_add_url_success_with_tags():
+    pass
+
+
+def test_add_seq_success_suffix():
+    pass
+
+
+def test_add_seq_success_image_dir():
+    pass
+
+
+def test_add_seq_success_suffix_and_dir():
+    pass
+
+
+def test_add_seq_fail_no_suffix():
+    pass
+
+
+def test_add_seq_success_new_feed():
+    pass
+
+
+def test_add_seq_success_over_max_explicit():
+    pass
+
+
+def test_add_seq_success_over_max_fnum():
+    pass
+
+
+def test_add_seq_success_tag_settings():
+    pass
+
+
+def test_add_random_success_with_max():
+    pass
+
+
+def test_add_random_success_with_dir():
+    pass
+
+
+def test_add_random_success_max_and_dir():
+    pass
+
+
+def test_add_seq_fail_no_suffix():
+    pass
+
+
+def test_add_random_fail_no_max():
+    pass
+
+
+def test_add_random_success_new_feed():
+    pass
+
+
+def test_add_random_success_max_one():
+    pass
+
+
+def test_add_random_success_tag_settings():
+    pass
